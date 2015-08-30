cb86c6bbd92451e7250e98f173d16a80336810b5
Nicholas Purdy <nicholaswpurdy@gmail.com>
2015-08-30 00:15:39 -0400

    ng-change added

--- controller.js
+++ controller.js
@@ -1,3 +1,19 @@
 app.controller('lineupController', function($scope) {
    
+   
+   $scope.radChange = function(valu){
+      console.log(valu);
+      
+      if(window.confirm("This will erase your current table. Continue?"))
+      {
+      
+      }
+      else
+      {
+         $scope.radio = ($scope.radio == "basketball" ? "football" : "basketball");
+         console.log($scope.radio);
+         
+      }
+   };
+   
 });
