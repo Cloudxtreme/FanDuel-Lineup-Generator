11af32c734aca5bc853bd15b5b89401bb08e2be0
Nicholas Purdy <nicholaswpurdy@gmail.com>
2015-08-31 11:20:48 -0400

    can switch between NBA and NFL now

--- js/formController.js
+++ js/formController.js
@@ -1,22 +1,36 @@
-app.controller('formController', ['$scope', function($scope) {
+app.controller('formController', ['$scope', '$http', function($scope, $http) {
+
+   $scope.radio = "basketball";
+   $scope.teams;
+   $scope.positions;
    
-   //$scope.fillForm($scope.radio);
+   var fillForm = function(sport){
+      $http.get("https://raw.githubusercontent.com/NicholasPurdy/Daily-Fantasy-Lineup-Generator/master/leagues.json")
+      .success(function(response){
+         if(sport === "basketball")
+         {
+            $scope.teams = response[0].teams;
+            $scope.positions = response[0].positions;
+         }
+         else
+         {
+            $scope.teams = response[1].teams;
+            $scope.positions = response[1].positions;
+         }
+      });
+   };
+   fillForm($scope.radio);
    
    $scope.radChange = function(sport){
       if(window.confirm("This will erase your current table. Continue?"))
       {
          //clearTable();
-         //fillForm(sport);
+         fillForm(sport);
       }
       else
       {
          $scope.radio = ($scope.radio == "basketball" ? "football" : "basketball");        
       }
-   };
-   
-   $scope.fillForm = function(sport){
-      //get json, fill form
-      console.log(sport);
    };
    
 }]);
