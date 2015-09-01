d6207d1d2c8bd532ebfcf9a14fdd3b9fe599d699
Nicholas Purdy <nicholaswpurdy@gmail.com>
2015-09-01 11:23:41 -0400

    new ng-models

--- js/formController.js
+++ js/formController.js
@@ -1,22 +1,19 @@
 app.controller('formController', ['$scope', '$http', function($scope, $http) {
 
-   $scope.radio;
-   $scope.teams;
-   $scope.positions;
-   
-   var fillForm = function(league){
-      $http.get("https://raw.githubusercontent.com/NicholasPurdy/Daily-Fantasy-Lineup-Generator/master/leagues.json")
-      .success(function(response){   
-         $scope.teams = response[league].teams;
-         $scope.positions = response[league].positions;
-      });
-   };
-   fillForm($scope.radio);
+   $scope.radio; // NBA or NFL
+   $scope.name;
+   $scope.team;
+   $scope.position; 
+   $scope.salary;
+   $scope.teams; // Array containing all teams
+   $scope.positions; // Array containing all positions
    
    $scope.radChange = function(league){
       if(window.confirm("This will erase your current table. Continue?"))
       {
          //clearTable();
+         $scope.name = "";
+         $scope.salary = "";
          fillForm(league);
       }
       else
@@ -25,4 +22,15 @@ app.controller('formController', ['$scope', '$http', function($scope, $http) {
       }
    };
    
+   var fillForm = function(league){
+      $http.get("https://raw.githubusercontent.com/NicholasPurdy/Daily-Fantasy-Lineup-Generator/master/leagues.json")
+      .success(function(response){   
+         $scope.teams = response[league].teams;
+         $scope.positions = response[league].positions;   
+         $scope.team = response[league].teams[0];
+         $scope.position = response[league].positions[0];
+      });
+   };
+   fillForm($scope.radio);
+ 
 }]);
