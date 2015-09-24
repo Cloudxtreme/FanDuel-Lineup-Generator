9c65cc8eefd41b339190116ba27a52b65ccef442
Nicholas Purdy <nicholaswpurdy@gmail.com>
2015-09-23 22:10:14 -0400

    insertRow added, but not tested, css fixed

--- js/tableService.js
+++ js/tableService.js
@@ -1,8 +1,14 @@
 app.factory('tableService', [function() {
    
-   var league = "NBA";
+   var league = "NBA"; //default value, may switch to NFL
    var table = [];
    var tables = [];
+   
+   var salary = 0;
+   var rulesData = {
+      teams: {},
+      playersOnTeam: {}
+   };
    
    function row (name, team, position, salary) {
       this.name = name;
@@ -11,28 +17,52 @@ app.factory('tableService', [function() {
       this.salary = salary;
    }
    
-   var insert = function(name, team, position, salary) {
-      table.push(new row(name, team, position, salary));
-   };
-   
-   var clearTable = function() {
-      table.length = 0;
-   };
-   
-   var setData = function(d) {
-      table = d.slice();
-      for(var i = 0; i < data.length; i++)
+   function insertRow(row) {
+      if(row.salary + salary <= 60000)
       {
-         table[i] = jQuery.extend(true, {}, table[i]);
-      }  
-   };
+         for (var teamUsed in rulesData.teams)
+         {
+            if(rulesData.teams.hasOwnProperty(teamUsed) &&
+            teamUsed < 3)
+            {
+               return false;
+            }
+         }
+         
+         for (var playersPerTeam in rulesData.playersOnTeam)
+         {
+            if(rulesData.playersOnTeam.hasOwnProperty(playersPerTeam) &&
+            playersPerTeam > 4)
+            {
+               return false;
+            }
+         }
+      
+         table.push(row);
+         rulesData.teams[row.team] += 1;
+         rulesData.playersOnTeam[row.team] += 1;
+         return true;
+      }
+   }
+   
+   function clearTable() {
+      table.length = 0;
+      tables.length = 0;
+      salary = 0;
+      rulesData.teams = {};
+      rulesData.playersOnTeam = {};
+   }
+   
+   function pop() {
+      table.pop();
+   }
    
    return {
       league: league,
       tables: tables,
-      insert: insert,
+      insertRow: insertRow,
       clearTable: clearTable,
-      setData: setData
+      pop: pop
    }
    
 }]);
