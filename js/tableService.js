b1aa58e0a149d84a27d0d880fe0b325c205c56e8
Nicholas Purdy <nicholaswpurdy@gmail.com>
2015-09-25 18:26:16 -0400

    rewrite of tableService

--- js/tableService.js
+++ js/tableService.js
@@ -7,60 +7,95 @@ function($http) {
    
    var salary = 0;
    var rules;
-   var rulesData = {};
    
    $http.get("https://raw.githubusercontent.com/NicholasPurdy/FanDuel-Lineup-Generator/master/rules.json")
    .success(function(response) {   
       rules = response;
    });
    
+   function getSalary() {
+      var salary = 0;
+      
+      for(var i = 0; i < table.length; i++)
+      {      
+         salary += table[i].salary;
+      }   
+      return salary;   
+   }
+   
+   function aSpotIsOpenFor(position) {
+      var j = 0; 
+      for(var i = 0; i < table.length; i++)
+      {
+         if(position === table[i].position)
+         {
+            j++;
+         }
+      }    
+      return(j < rules[position]);
+   } 
+
+   function teamsRepresented() {
+      var teams = {};
+      for(var i = 0; i < table.length; i++)
+      {
+         teams[table[i].team];
+      }
+      return (Object.key(teams).length);
+   }
+   
+   function fourOrLessRule() {
+      var playersOnTeam = {};
+      for(var i = 0; i < table.length; i++)
+      {
+         if(playersOnTeam.hasOwnProperty(table[i].team))
+         {
+            playersOnTeam[table[i].team]++;
+         }
+         else
+         {
+            playersOnTeam[table[i].team] = 1;
+         }
+      }
+      for(var team in playersOnTeam)
+      {
+         if(playersOnTeam.hasOwnProperty(team) && team > 4)
+         {
+            return false;
+         }
+      }
+      
+      return true;
+   }
+   
    function row (name, team, position, salary) {
       this.name = name;
       this.team = team;
       this.position = position;
       this.salary = salary;
-   }
-   
-   function aSpotIsOpenFor(position) {
-      var i = 0;
-   
-      for(var row in table)
-      {
-         if(row.position === position)
-         {
-            i++;
-         }
-      }
-      
-      if(i < rules[position])
-      {
-         return true;
-      }
-      
-      return false;
-   }
+   }   
    
    function insertRow(row) {
-      if(row.salary + salary <= 60000 && aSpotIsOpenFor(row.position))
+      if(((row.salary + getSalary()) <= 60000) && (aSpotIsOpenFor(row.position)))
       {
          table.push(row);
-         rulesData[row.team] += 1;
          return true;
-      }
-      
+      }     
       return false;
    }
    
    function insertTable() {
-      
+      if((teamsRepresented() >= 3) && fourOrLessRule())
+      {  
+         tables.push(table);
+         return true;
+      }    
+      return false;
    }
    
    function clearTable() {
       table.length = 0;
       tables.length = 0;
-      salary = 0;
-      rulesData.teams = {};
-      rulesData.playersOnTeam = {};
    }
    
    function pop() {
@@ -71,6 +106,7 @@ function($http) {
       league: league,
       tables: tables,
       insertRow: insertRow,
+      insertTable: insertTable,
       clearTable: clearTable,
       pop: pop
    }
